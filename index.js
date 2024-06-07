1c4721de3ae1239cb5183cd001bb1408608f5ed4
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-06-07 11:59:24 +0500

    Sigma5.10

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ button.addEventListener("click", (event) => {
 
     let vazifa = input.value;
     let li = document.createElement("li");
+    let span = document.createElement("span")
 
-    li.textContent = vazifa;
+    let done = document.createElement("button")
+    let del = document.createElement("button")
+
+    done.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        span.classList.toggle("line");
+    });
+
+    del.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        li.remove();
+    });
+
+    span.textContent = vazifa;
+    done.textContent = "Done";
+    del.textContent = "Delete";
+
+    done.classList.add("done")
+    del.classList.add("del")
+
+    li.appendChild(span);
+    li.appendChild(done);
+    li.appendChild(del);
 
     list.appendChild(li);
 
